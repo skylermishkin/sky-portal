bc971cc768b3d0cf2cb481b1432925001f31bc76
skylermishkin <meestermishkin@gmail.com>
2015-11-03 15:40:29 -0700

    changed the content dir to src/content

--- content/nav-content.js
+++
@@ -1,9 +0,0 @@
-/**
- * Exports an object with content to display in the nav bar.
- */
-
-export default {
-    nodes: [{title: 'Home', hyperlink: ''},
-        {title: 'Ongoing Work', hyperlink: ''},
-        {title: 'Completed Projects', hyperlink: ''},],
-}
